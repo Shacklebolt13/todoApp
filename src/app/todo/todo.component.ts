17047c98cf185c9b4a9843b4d12f23f9eebc198c
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-08-04 12:32:36 +0530

    add todo support

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { Todo } from "./todo-item/Item";
 
 @Component({
@@ -10,12 +10,15 @@ import { Todo } from "./todo-item/Item";
 export class TodoComponent implements OnInit {
 
   todos: Todo[];
+  doneStatus: boolean = false;
+  @ViewChild("in_title") in_title!: ElementRef;
+  @ViewChild("in_done") in_done!: ElementRef;
+  @ViewChild("in_desc") in_desc!: ElementRef;
 
   constructor() {
     this.todos = [
 
       new Todo(1, "hello", "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem quisquam. ", false),
-
     ];
   }
 
@@ -24,6 +27,23 @@ export class TodoComponent implements OnInit {
   destroyTodoHandlerParent(todo: Todo): void {
     console.log("destroyTodoCaught");
     this.todos = this.todos.filter(t => t !== todo);
+  }
+
+  toggleFormDone(): void {
+    this.doneStatus = !this.doneStatus;
+  }
+
+  addTodo() {
+    console.log("addTodo");
+    console.log(this.in_title.nativeElement.value);
+    console.log(this.in_done.nativeElement.checked);
+    console.log(this.in_desc.nativeElement.value);
+
+    this.todos.push(new Todo(1, this.in_title.nativeElement.value, this.in_desc.nativeElement.value, this.in_done.nativeElement.checked));
+    this.in_title.nativeElement.value = "";
+    this.in_desc.nativeElement.value = "";
+    this.in_done.nativeElement.checked = false;
+
   }
 }
 
